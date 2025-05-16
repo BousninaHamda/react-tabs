c5f13ee3171d788a617520d00fdc4786ee34e9b4
vynas <hamdabousnina@gmail.com>
2025-05-16 17:46:22 -0400

    fetch and navigate job data

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,55 @@
-const url = "https://course-api.com/react-tabs-project";
+import { useEffect } from "react";
+import { useState } from "react";
+import JobInfo from "./JobInfo";
+import BtnContainer from "./BtnContainer";
+
+const url = "https://www.course-api.com/react-tabs-project";
 
 function App() {
-  return <h2>Tabs</h2>;
+  const [isLoading, setIsLoading] = useState(true);
+  const [isError, setIsError] = useState(false);
+  const [jobs, setJobs] = useState([]);
+  const [currentItem, setCurrentItem] = useState(0);
+
+  const fetchJobs = async () => {
+    const res = await fetch(url);
+    const newJobs = await res.json();
+    setJobs(newJobs);
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    try {
+      fetchJobs();
+    } catch (error) {
+      setIsError(true);
+    }
+  }, []);
+
+  if (isLoading) {
+    return (
+      <section className="jobs-center">
+        <div className="loading"></div>
+      </section>
+    );
+  }
+  if (isError) {
+    return (
+      <section className="jobs-center">
+        <h2>Error Occurred</h2>
+      </section>
+    );
+  }
+  return (
+    <section className="jobs-center">
+      <BtnContainer
+        jobs={jobs}
+        currentItem={currentItem}
+        setCurrentItem={setCurrentItem}
+      />
+      <JobInfo jobs={jobs} currentItem={currentItem} />
+    </section>
+  );
 }
 
 export default App;
